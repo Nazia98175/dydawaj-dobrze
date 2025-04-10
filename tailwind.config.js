8d02583cd41100741c032b4bef6b51a72a2133cf
rohit <rohit780790809@gmail.com>
2025-04-10 16:13:45 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+
+        "3xl": "1720px",
+        // => @media (min-width: 1536px) { ... }
+      },
+      colors: {
+        primary: "#FFEA74",
+        secondry: "#F50269",
+        skyblue: "#3CF",
+        lightblack: "#243237",
+        exodusFruit: "#696ADB",
+      },
+    },
+  },
+  plugins: [],
+};
