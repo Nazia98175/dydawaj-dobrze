ccde023ee6b0a3523e15a93ff3c49e0218d4cda5
rohit <rohit780790809@gmail.com>
2025-04-24 14:03:49 +0530

    fix code

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,16 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+   build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        shopcard: "shopcard.html",
+        guides: "guides.html",
+        about: "about.html",
+        special: "special.html",
+        opportunities: "opportunities.html",
+      },
+    },
+  },
 });
